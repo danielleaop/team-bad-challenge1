aedcde106e405930604d006e20c50438959457e8
Danielle <168420921+danielleaop@users.noreply.github.com>
2024-09-03 12:13:22 +0100

    lint test part 4

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { Intern } from "next/font/google";
 import "./globals.css";
 
 var inter = Inter({ subsets: ['latin'] });
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{childKren}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
